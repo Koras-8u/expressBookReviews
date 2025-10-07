c9903dade708790534b4160e7ba8da8d199772b5
Kevin García Morales <kevinmor2099@gmail.com>
2025-10-07 14:18:32 -0600

    REFACTOR: Use Axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,6 +2,7 @@ const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+const axios = require("axios");
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
@@ -21,88 +22,86 @@ public_users.post("/register", (req, res) => {
   return res.status(201).json({ message: "User registered successfully." });
 });
 
+public_users.get("/booksdb", function (req, res) {
+  res.status(200).json(books);
+});
+
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
   console.log("START - Get list of books");
-  new Promise((resolve, reject) => {
-    if (!books || Object.keys(books).length === 0)
-      reject({ status: 404, message: "No books available" });
-    else resolve(books);
-  })
-    .then((result) => {
+  axios
+    .get("http://localhost:5000/booksdb")
+    .then((response) => {
       console.log("END - Get list of books");
-      res.status(200).json(result);
+      res.status(200).json(response.data);
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(err.status).json({ message: err.message });
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: "Error fetching books" });
     });
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   console.log("START - Get books by ISBN");
-  new Promise((resolve, reject) => {
-    const book = books[req.params.isbn];
-    if (!book) {
-      reject({ status: 404, message: "Book not found" });
-    } else {
-      resolve(book);
-    }
-  })
-    .then((result) => {
-      console.log("END - Get books by ISBN");
-      res.status(200).json(result);
+  axios
+    .get("http://localhost:5000/booksdb")
+    .then((response) => {
+      const book = response.data[req.params.isbn];
+      if (!book) {
+        res.status(404).json({ message: "Book not found" });
+      } else {
+        console.log("END - Get books by ISBN");
+        res.status(200).json(book);
+      }
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(err.status).json({ message: err.message });
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: "Error fetching books" });
     });
 });
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
   console.log("START - Get books by author");
-  new Promise((resolve, reject) => {
-    const booksByAuthor = Object.values(books).filter(
-      (book) => book.author === req.params.author
-    );
-    if (booksByAuthor.length === 0) {
-      reject({ status: 404, message: "Books not found" });
-    } else {
-      resolve(booksByAuthor);
-    }
-  })
-    .then((result) => {
-      console.log("END - Get books by author");
-      res.status(200).json(result);
+  axios
+    .get("http://localhost:5000/booksdb")
+    .then((response) => {
+      const booksByAuthor = Object.values(response.data).filter(
+        (book) => book.author === req.params.author
+      );
+      if (booksByAuthor.length === 0) {
+        res.status(404).json({ message: "Books not found" });
+      } else {
+        console.log("END - Get books by author");
+        res.status(200).json(booksByAuthor);
+      }
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(err.status).json({ message: err.message });
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: "Error fetching books" });
     });
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   console.log("START - Get books by title");
-  new Promise((resolve, reject) => {
-    const booksByTitle = Object.values(books).filter(
-      (book) => book.title === req.params.title
-    );
-    if (booksByTitle.length === 0) {
-      reject({ status: 404, message: "Books not found" });
-    } else {
-      resolve(booksByTitle);
-    }
-  })
-    .then((result) => {
-      console.log("END - Get books by title");
-      res.status(200).json(result);
+  axios
+    .get("http://localhost:5000/booksdb")
+    .then((response) => {
+      const booksByTitle = Object.values(response.data).filter(
+        (book) => book.title === req.params.title
+      );
+      if (booksByTitle.length === 0) {
+        res.status(404).json({ message: "Books not found" });
+      } else {
+        console.log("END - Get books by title");
+        res.status(200).json(booksByTitle);
+      }
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(err.status).json({ message: err.message });
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: "Error fetching books" });
     });
 });
 
